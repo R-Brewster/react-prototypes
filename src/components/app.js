405dd3d3358f2e818a4c414a8f403910409b80c0
R-Brewster <rbrewster@ucmerced.edu>
2017-10-24 21:52:27 -0700

    Completed prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,31 @@
+import React from 'react';
+import Table from './table.js';
+
+const students = [
+    {
+        student: 'Peter Parker',
+        course: 'Intro to Photography',
+        grade: 85
+    },
+
+    {
+        student: 'Bruce Banner',
+        course: 'Psychology',
+        grade: '91'
+    },
+    {
+        student: 'Tony Stark',
+        course: 'Mechanical Engineering',
+        grade: '100'
+    },
+];
+
+export default () => {
+    return (<div className = "container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
 
 
